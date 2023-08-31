375189f265a0acb58fe56cd3513fd10931face30
yeji <yejipooh@gmail.com>
2023-08-31 16:33:11 +0900

    js practice

--- 8월30일/실습1/main3.js
+++ 8월30일/실습1/main3.js
@@ -1,3 +1,5 @@
+const Btn = document.getElementsByClassName('btn');
+
 const data = ['번호', '이름', '국어', '영어', '수학'];
 
 function Student(number, name, kor, eng, math) {
@@ -14,25 +16,70 @@ let stu = [
   new Student(3, '정은', 90, 30, 40, 80),
 ];
 
-document.write('<table class="table table-color">');
+var inputValue1 = document.getElementById('input1');
+var inputValue2 = document.getElementById('input2');
+var inputValue3 = document.getElementById('input3');
+var inputValue4 = document.getElementById('input4');
+
+function addNewNode() {
+  let newNode = '';
+  for (var i = 0; i < stu.length; i++) {
+    newNode += '<tr>';
+    newNode += `<td>${i + 1}</td>`;
+    newNode += `<td>${stu[i].name}</td>`;
+    newNode += `<td>${stu[i].kor}</td>`;
+    newNode += `<td>${stu[i].eng}</td>`;
+    newNode += `<td>${stu[i].math}</td>`;
+    newNode += '</tr>';
+  }
+  return newNode;
+}
+
+function handleBtn(e) {
+  // e.preventDefault();
+
+  stu.push(
+    new Student(
+      4,
+      inputValue1,
+      Number(inputValue2),
+      Number(inputValue3),
+      Number(inputValue4)
+    )
+  );
+  console.log('stu', stu);
+
+  let num = 3;
+  const TrData = document.getElementById('trData');
+  const tbodyTag = document.createElement('tr');
+
+  // tbodyTag.innerHTML = `<td>${(num += 1)}</td><td>${
+  //   inputValue1.value
+  // }</td><td>${inputValue2.value}</td><td>${inputValue3.value}</td><td>${
+  //   inputValue4.value
+  // }</td>`;
+  // TrData.appendChild(tbodyTag);
+
+  // TrData.appendChild(newNode);
+
+  TrData.innerHTML = addNewNode();
+  console.log('new', TrData);
+
+  inputValue1.value = '';
+  inputValue2.value = '';
+  inputValue3.value = '';
+  inputValue4.value = '';
+}
+
+document.write('<table class="table table-striped table-hover">');
 document.write(' <thead>');
 document.write(' <tr>');
 for (let i = 0; i < data.length; i++) {
   document.write(`<th scope="col">${data[i]}</th>`);
 }
 document.write(' </tr>');
-document.write(' <tr>');
-
-for (var i = 0; i < stu.length; i++) {
-  document.write(' <tr>');
-  document.write(`<td>${stu[i].number}</td>`);
-  document.write(`<td>${stu[i].name}</td>`);
-  document.write(`<td>${stu[i].kor}</td>`);
-  document.write(`<td>${stu[i].eng}</td>`);
-  document.write(`<td>${stu[i].math}</td>`);
-  document.write('</tr>');
-}
-
-document.write('     </tr>');
+document.write(' </thead>');
+document.write('  <tbody id="trData">');
+document.write(addNewNode());
 document.write('  </tbody>');
 document.write(' </table>');
